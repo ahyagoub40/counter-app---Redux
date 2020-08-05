93d90bd30e9e5f9ac64311fd64280ad0dd367883
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-08-05 16:57:51 -0600

    refactored code

--- src/containers/counter.js
+++ src/containers/counter.js
@@ -1,20 +1,8 @@
-import React from 'react'
 import { connect } from "react-redux";
 import { add, subtract, increment, decrement } from '../reducer/action'
 
-const Counter = function (props) {
-  console.log('props', props)
-  return (
-    <div>
-      <h1>Counter</h1>
-      <h2>{props.counter}</h2>
-      <button onClick={() => props.increase()}>Increment</button>
-      <button onClick={() => props.decrease()}>Decrement</button>
-      <button onClick={() => props.addvalue()}>Add 5</button>
-      <button onClick={() => props.subtractvalue()}>Subtract 5</button>
-    </div>
-  )
-}
+import Counter from '../component/counter'
+
 const mapStateToProps = state => ({
   counter: state.counter
 })
